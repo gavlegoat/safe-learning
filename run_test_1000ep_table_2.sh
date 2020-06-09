0f6fef94a5ec35aa6e3825fef5daa9a66f91cf94
Greg <ganderso@cs.utexas.edu>
2020-06-09 12:31:38 -0500

    Code submission

--- run_test_1000ep_table_2.sh
+++
@@ -1,4 +0,0 @@
-python benchmarks/cartpole_change_l.py --nn_test --shield_test --test_episodes=1000
-python benchmarks/pendulum_change_m.py --nn_test --shield_test --test_episodes=1000
-python benchmarks/pendulum_change_l.py --nn_test --shield_test --test_episodes=1000
-python benchmarks/selfdriving_obs.py --nn_test --shield_test --test_episodes=1000
--- run_test_100ep_table_2.sh
+++
@@ -1,4 +0,0 @@
-python benchmarks/cartpole_change_l.py --nn_test --shield_test
-python benchmarks/pendulum_change_m.py --nn_test --shield_test
-python benchmarks/pendulum_change_l.py --nn_test --shield_test
-python benchmarks/selfdriving_obs.py --nn_test --shield_test
